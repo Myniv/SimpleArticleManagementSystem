064624f9fdf626cd2f940f53298d071eaff91819
Mulyana N <mulyanan.ur26@gmail.com>
2025-02-12 15:22:19 +0700

    create header footer and master

---
+++ app/Views/layout/footer.php
@@ -0,0 +1,16 @@
+</main>
+<footer class="p-3 bg-dark text-white mt-auto">
+    <div class="container">
+        <p class="mb-1 text-center">`Copyright © <?= date('Y') ?> Mulyana N. All Rights Reserved`</p>
+        <p class="mb-0 text-center">
+            <a href="https://www.linkedin.com/in/mulyana-nurcahyani-45216b267/">
+                Linkedin
+            </a>
+            &ensp;
+            <a href="https://www.instagram.com/mulyanan_/">Instagram</a>
+        </p>
+    </div>
+</footer>
+</body>
+
+</html>
---
+++ app/Views/layout/master.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<div class="d-flex flex-column min-vh-100">
+    <?= $this->include('layout/header') ?>
+    <div class="flex-grow-1">
+        <?= $this->renderSection('content') ?>
+    </div>
+    <?= $this->include('layout/footer') ?>
+</div>
