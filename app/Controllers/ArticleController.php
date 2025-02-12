24c17e5f14fc758cf5f6c97e6ce4057c009b6fe3
Mulyana N <mulyanan.ur26@gmail.com>
2025-02-12 15:17:29 +0700

    Create article controller

---
+++ app/Controllers/ArticleController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Entities\Article;
+use App\Models\M_Article;
+
+class ArticleController extends BaseController
+{
+    private $articleModel;
+
+    public function __construct()
+    {
+        $this->articleModel = new M_Article();
+    }
+    public function index()
+    {
+        $data = $this->articleModel->getAllArticle();
+        return view('article/v_article_list', $data);
+    }
+
+    public function create()
+    {
+        $type = $this->request->getMethod();
+        if ($type == "GET") {
+            return view("article/v_create");
+        }
+
+        $data = [
+            'id' => $this->request->getPost("id"),
+            'title' => $this->request->getPost("title"),
+            'content' => $this->request->getPost("content")
+        ];
+        $rule = [
+            'id' => 'required|integer',
+            "title" => 'required|max_length[255]',
+            'content' => 'required|max_length[255]'
+        ];
+
+        if (!$this->validateData($data, $rule)) {
+            return view("article/v_article_create", ['errors' => $this->validator->getErrors()]);
+        }
+
+        $article = new Article($data['id'], $data['title'], $data['content']);
+        $this->articleModel->addArticle($article);
+        return redirect()->to("/article");
+    }
+
+    public function update($id)
+    {
+        $type = $this->request->getMethod();
+        if ($type == "GET") {
+            $data['article'] = $this->articleModel->getArticleById($id);
+            return view("article/v_create", $data);
+        }
+
+        $data = [
+            'id' => $this->request->getPost("id"),
+            'title' => $this->request->getPost("title"),
+            'content' => $this->request->getPost("content")
+        ];
+        $rule = [
+            'id' => 'required|integer',
+            "title" => 'required|max_length[255]',
+            'content' => 'required|max_length[255]'
+        ];
+
+        if (!$this->validateData($data, $rule)) {
+            return view("article/v_article_create", ['errors' => $this->validator->getErrors()]);
+        }
+
+        $article = new Article($data['id'], $data['title'], $data['content']);
+        $this->articleModel->updateArticle($article);
+        return redirect()->to("article");
+    }
+
+    public function delete($id)
+    {
+        $article = $this->articleModel->deleteArticle($id);
+        return redirect()->to("/article");
+    }
+}
