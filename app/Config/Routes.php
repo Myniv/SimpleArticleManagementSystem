ccdcd7bc30d423deb7d5a2709f63d7b416d92f6b
Mulyana N <mulyanan.ur26@gmail.com>
2025-02-12 18:06:07 +0700

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,8 +1,22 @@
 <?php
 
+use App\Controllers\ArticleController;
 use CodeIgniter\Router\RouteCollection;
 
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->addRedirect('/home', '/');
+
+$routes->group('article', function ($routes) {
+    $routes->get('/', [ArticleController::class, 'index']);
+    $routes->get('detail/(:segment)', [ArticleController::class, 'detail']);
+    $routes->match(['get', 'post'], 'create', [ArticleController::class, 'create']);
+    $routes->match(['get', 'put'], 'update/(:segment)', [ArticleController::class, 'update']);
+    $routes->delete('delete/(:num)', [ArticleController::class, 'delete/$1']);
+});
+
+// $routes->set404Override(static function () {
+//     return view("errors/custom_error_404");
+// });
